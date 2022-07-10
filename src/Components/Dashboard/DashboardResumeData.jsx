031cb780f6ef3d5e837028bca4c6ebc895071e5f
lusteroak <lusteroak@protonmail.com>
2022-07-10 16:03:49 -0400

    Initialized Dashboard Development

---
+++ src/Components/Dashboard/DashboardResumeData.jsx
@@ -0,0 +1,23 @@
+export const DashboardResumeData = [
+  {
+    id: 1,
+    icon: <FaHome />,
+    text_detail: 'Views',
+    number: 250,
+    text_frequency: 'Per Day',
+  },
+  {
+    id: 2,
+    icon: <FaHome />,
+    text_detail: 'Visits',
+    number: 1300,
+    text_frequency: 'Per Minutes',
+  },
+  {
+    id: 3,
+    icon: <FaHome />,
+    text_detail: 'Orders',
+    number: 250,
+    text_frequency: 'Per Day',
+  },
+];
---
+++ src/Components/SideBarNavigation/SideBarElements.jsx
@@ -0,0 +1,41 @@
+import {
+  FaHome,
+  FaTrophy,
+  FaUniversity,
+  FaUserAlt,
+  FaPoll,
+  FaTools,
+} from 'react-icons/fa';
+
+export const SideBarElements = [
+  {
+    id: 1,
+    navigate: 'Home',
+    icon: <FaHome />,
+  },
+  {
+    id: 2,
+    navigate: 'Projects',
+    icon: <FaTrophy />,
+  },
+  {
+    id: 3,
+    navigate: 'Billings',
+    icon: <FaUniversity />,
+  },
+  {
+    id: 4,
+    navigate: 'Team Members',
+    icon: <FaUserAlt />,
+  },
+  {
+    id: 5,
+    navigate: 'Stats',
+    icon: <FaPoll />,
+  },
+  {
+    id: 6,
+    navigate: 'Settings',
+    icon: <FaTools />,
+  },
+];
