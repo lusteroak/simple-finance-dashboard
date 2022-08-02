f1f2081aaff8f2880905b789db946734b8880bf2
lusteroak <lusteroak@protonmail.com>
2022-08-02 16:38:35 -0400

    Dashboard Detailed Added

---
+++ src/Components/Dashboard/DashboardDetailedData.jsx
@@ -0,0 +1,30 @@
+export const DashboardDetailedData = [
+  {
+    id: 1,
+    type: 'Sales',
+    text_detail: 'Total Sales Today',
+    number: 500,
+    percent: 55,
+  },
+  {
+    id: 2,
+    type: 'Profit',
+    text_detail: 'Per Day Ratio',
+    number: 500,
+    percent: 55,
+  },
+  {
+    id: 3,
+    type: 'Orders',
+    text_detail: 'Total Order Today',
+    number: 500,
+    percent: 55,
+  },
+  {
+    id: 4,
+    type: 'Visits',
+    text_detail: 'Total Visit Today',
+    number: 500,
+    percent: 55,
+  },
+];
